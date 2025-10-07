9604e94435515377e10d5699b76eba547661a3d4
Alejandro <171895343+jAlejandroGM@users.noreply.github.com>
2025-10-07 18:59:23 -0500

    feat: implement internationalization support with i18next and add translations for Topbar component

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { FaWhatsapp, FaEnvelope, FaPhone } from "react-icons/fa";
 import styles from "./Topbar.module.css";
+import { useTranslation } from "react-i18next";
 
 function Topbar() {
+  const { t } = useTranslation();
+
   return (
     <div className="bg-black text-white py-1 d-none d-md-block">
       <div className="container d-flex flex-column flex-md-row justify-content-between align-items-center gap-2">
@@ -14,7 +17,7 @@ function Topbar() {
             <FaEnvelope color="white" size={14} />
             <a
               href="mailto:contacto@alcooling.com"
-              aria-label="Enviar correo a contacto@alcooling.com"
+              aria-label={t("topbar.email-aria-label")}
             >
               contacto@alcooling.com
             </a>
@@ -22,7 +25,7 @@ function Topbar() {
           |
           <span className="d-flex align-items-center gap-1">
             <FaPhone color="white" size={14} />
-            <a href="tel:+51999999999" aria-label="Llamar al +51 999 999 999">
+            <a href="tel:+51999999999" aria-label={t("topbar.tel-aria-label")}>
               +51&nbsp;999&nbsp;999&nbsp;999
             </a>
           </span>
@@ -32,8 +35,8 @@ function Topbar() {
             target="_blank"
             rel="noopener noreferrer"
             className={`d-flex align-items-center gap-1 rounded-pill ${styles["wsp-btn"]}`}
-            aria-label="Chatea por WhatsApp"
-            title="Chatea por WhatsApp"
+            aria-label={t("topbar.wsp-aria-label")}
+            title={t("topbar.wsp-aria-label")}
           >
             <FaWhatsapp size={18} />
             WhatsApp
